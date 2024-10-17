255e3f78e57e0192b3f7960bad454d7d3ae89362
Luis Fellipe Ricardo Lisboa <20211204010017@ifrn.local>
2024-10-17 13:21:10 -0300

    Criando o Controller de Eventos

---
+++ src/main/java/pi/luis/eventos/controllers/eventosController.java
@@ -0,0 +1,13 @@
+package pi.luis.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class eventosController {
+	
+	@RequestMapping("/eventos/form")
+	public String form(){
+		return("formEvento");
+	}
+}
