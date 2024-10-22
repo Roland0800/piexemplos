3c9b4c5ef4af3808e496552bf316162ca3e72471
Luis Fellipe Ricardo Lisboa <20211204010017@ifrn.local>
2024-10-22 09:39:33 -0300

    Tentando fazer funcionar a página final do formulário

--- src/main/java/pi/luis/eventos/controllers/eventosController.java
+++ src/main/java/pi/luis/eventos/controllers/eventosController.java
@@ -1,6 +1,8 @@
 package pi.luis.eventos.controllers;
 
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,10 +13,19 @@ public class eventosController {
 	@RequestMapping("/eventos/form")
 	public String form(){
 		return("formEvento");
+		
 	}
 	
 	@RequestMapping("/eventos/respForm")
 	public String respForm(){
+		String nome;
+		String local;
+		String data;
+		String horario;
+		System.out.println(nome);
+		System.out.println(local);
+		System.out.println(data);
+		System.out.println(horario);
 		return("respForm");
 	}
 }
